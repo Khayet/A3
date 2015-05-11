ab04cea087adf8a29ecd45cd5bca96adc406c730
Khayet <jakob.herpel@uni-weimar.de>
2015-05-11 16:41:46 +0200

    aufgabe 3.3

--- aufgabe2bis4.cpp
+++ aufgabe2bis4.cpp
@@ -4,13 +4,14 @@
 #include <iostream> 
 #include <iterator>
 #include <algorithm>
+#include <map>
 
 int main()
 {
   std::list<unsigned int> list1(100);
 
   for (std::list<unsigned int>::iterator i = list1.begin(); i != list1.end(); ++i) {
-    *i = rand() % 50;
+    *i = std::rand() % 50;
   }
 
   std::vector<unsigned int> vec1(100);
@@ -19,13 +20,13 @@ int main()
   std::copy(vec1.begin(), vec1.end(), std::ostream_iterator<unsigned int>(std::cout, "\n"));
   std::cout << "-------------------" << std::endl;
 
-  for (int i = 0; i <= 50; ++i) {
-    for (int j : vec1) {
-      if (vec1[j] == i) vec1.erase(vec1.begin() + i); //TODO: FIX BUG PLOX
-    }
+  std::map<unsigned int,int> ints{};
+  for (unsigned int i{0}; i < vec1.size(); ++i) {
+    ints.insert(std::pair<unsigned int, int>(vec1[i], 0));
   }
 
-  std::copy(vec1.begin(), vec1.end(), std::ostream_iterator<unsigned int>(std::cout, "\n")); 
+  int numbersCount{0};
+  std::cout << ints.size() << std::endl;
 
   return 0;
 }
